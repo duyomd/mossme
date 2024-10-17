6d50a30c0ca1ec4ee89c5d91b0bb498e5aee0979
duyomd <duyomd@gmail.com>
2024-10-17 15:02:52 +0700

    css load tuning

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -22,19 +22,22 @@
   <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" 
+    rel="stylesheet" media="none" onload="this.media='all'">
 
   <!-- Vendor CSS Files -->
-  <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/css/critical.css" rel="stylesheet">
+  <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet" media="none" onload="this.media='all'">
+  <link href="/assets/vendor/aos/aos.css" rel="stylesheet" media="none" onload="this.media='all'">  
   <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" media="none" onload="this.media='all'">
+  <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" media="none" onload="this.media='all'">
+  <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" media="none" onload="this.media='all'">
   <!-- RTL CSS -->
   <?php if (App\Helpers\Utilities::isRightToLeft()) : ?>
     <link href="/assets/vendor/bootstrap/css/bootstrap.rtl.min.css" rel="stylesheet">
+  <?php else : ?>
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <?php endif ?>
 
   <!-- Template Main CSS File -->
